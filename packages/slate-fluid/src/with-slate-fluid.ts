a55c43ac7c5fdead6b27a79fa00c9688f10a557b
Yangyang Li <mqyangyang521@qq.com>
2020-09-24 12:17:17 +0800

    feat(slate-fluid): add slate fluid middleware

---
+++ packages/slate-fluid/src/with-slate-fluid.ts
@@ -0,0 +1,15 @@
+import {Editor, Operation} from 'slate'
+import {BaseFluidModel, IOperation} from "@solidoc/fluid-model-base";
+
+const withSlateFluid = <T extends Editor, U extends BaseFluidModel>(editor: T, model: U) => {
+    const {apply} = editor;
+
+    editor.apply = async (op: Operation) => {
+        await model.apply(op as any)
+        apply(op)
+    }
+
+    model.subscribe('id')
+    return editor
+}
+export {withSlateFluid}
