e9941541d5a4455087f3f24a20443117fb4d2af8
Yangyang Li <mqyangyang521@qq.com>
2020-09-25 15:21:54 +0800

    fix(test): fixed node build and add test for insert text
    fix all package fluid version to 0.27.0 & add insert node operation test

--- packages/fluid-model-slate/src/operation-applier/element-factory.ts
+++ packages/fluid-model-slate/src/operation-applier/element-factory.ts
@@ -1,15 +1,29 @@
 import { Node } from 'slate';
 import { IFluidDataStoreRuntime } from '@fluidframework/datastore-definitions';
-import { SharedObjectSequence } from '@fluidframework/sequence';
+import {SharedObjectSequence, SharedString} from '@fluidframework/sequence';
 import { SharedMap } from '@fluidframework/map';
 import {FLUIDNODE_KEYS} from "../interfaces";
 
+const createText = (text: string, runtime: IFluidDataStoreRuntime) => {
+  const shareString = SharedString.create(runtime);
+  shareString.insertText(0, text)
+  return shareString
+}
+
+const createChildren = (runtime: IFluidDataStoreRuntime) => {
+  return SharedObjectSequence.create(runtime);
+}
+
 const create = (node: Node, runtime: IFluidDataStoreRuntime) => {
   const element = SharedMap.create(runtime);
-  const children = SharedObjectSequence.create(runtime);
-  element.set(FLUIDNODE_KEYS.TEXT, node.text);
+
+  const children = createChildren(runtime);
+  const text = createText(node.text as string, runtime);
+
+  element.set(FLUIDNODE_KEYS.TEXT, text.handle);
   element.set(FLUIDNODE_KEYS.CHILDREN, children.handle);
+
   return element;
 };
 
-export { create };
+export { create, };
--- packages/fluid-model-slate/test/operation-applier/operation-applier.spec.ts
+++ packages/fluid-model-slate/test/operation-applier/operation-applier.spec.ts
@@ -1,6 +1,10 @@
 import * as mocks from "@fluidframework/test-runtime-utils";
-import {InsertNodeOperation} from "slate";
-import {SharedObjectSequence, SharedObjectSequenceFactory} from "@fluidframework/sequence";
+import {
+    SharedObjectSequence,
+    SharedObjectSequenceFactory,
+    SharedString,
+    SharedStringFactory
+} from "@fluidframework/sequence";
 import {SharedMap} from "@fluidframework/map";
 import {IFluidHandle} from "@fluidframework/core-interfaces";
 
@@ -10,29 +14,44 @@ import {getNode} from "../../src/operation-applier/node-getter";
 import {operationApplier} from "../../src/operation-applier";
 import {FLUIDNODE_KEYS} from "../../src/interfaces";
 
+SharedString.create = (runtime) => new SharedString(runtime, uuid.v4(), SharedStringFactory.Attributes)
 SharedMap.create = (runtime) => new SharedMap(uuid.v4(), runtime, SharedMap.getFactory().attributes)
 SharedObjectSequence.create = (runtime) => new SharedObjectSequence(runtime, uuid.v4(), SharedObjectSequenceFactory.Attributes);
 
 describe('operation applier', () => {
-    const mockRuntime: mocks.MockFluidDataStoreRuntime = new mocks.MockFluidDataStoreRuntime();
+    const mockRuntime: mocks.MockFluidDataStoreRuntime = new mocks.MockFluidDataStoreRuntime()
     const initNode = (childrenNode: SharedMap[] = []) => {
         const node = new SharedMap(uuid.v4(), mockRuntime, SharedMap.getFactory().attributes)
-        const childSequence = new SharedObjectSequence<IFluidHandle<SharedMap>>(mockRuntime, uuid.v4(), SharedObjectSequenceFactory.Attributes);
 
+        const childSequence = new SharedObjectSequence<IFluidHandle<SharedMap>>(mockRuntime, uuid.v4(), SharedObjectSequenceFactory.Attributes)
         childSequence.insert(0, childrenNode.map((v, i) => <IFluidHandle<SharedMap>>v.handle))
+
+        const text = new SharedString(mockRuntime, uuid.v4(), SharedStringFactory.Attributes)
+        text.insertText(0, "This default text")
+
         node.set(FLUIDNODE_KEYS.CHILDREN, childSequence.handle)
+        node.set(FLUIDNODE_KEYS.TEXT, text.handle)
 
         return node
     }
+
+    const initEditorRoot = () => {
+        const root = new SharedObjectSequence<IFluidHandle<SharedMap>>(mockRuntime, uuid.v4(), SharedObjectSequenceFactory.Attributes)
+        const node_0_0 = initNode()
+        const node_0 = initNode([node_0_0])
+        root.insert(0, [<IFluidHandle<SharedMap>>node_0.handle])
+        return root
+    }
+
+    const getNodeText = async (root:SharedObjectSequence<IFluidHandle<SharedMap>>, path: number[]): Promise<string> => {
+        const resultNode = await getNode(path, root)
+        const textHandle = resultNode.get<IFluidHandle<SharedString>>(FLUIDNODE_KEYS.TEXT)
+        const expectShareString = await textHandle.get();
+        return expectShareString.getText()
+    }
     describe('insert node operation', () => {
         it('should insert a element to path when apply a insert node op', async () => {
-            const root = new SharedObjectSequence<IFluidHandle<SharedMap>>(mockRuntime, "id", SharedObjectSequenceFactory.Attributes)
-
-            const node_0_0 = initNode()
-
-            const node_0 = initNode([node_0_0])
-
-            root.insert(0, [<IFluidHandle<SharedMap>>node_0.handle])
+            const root = initEditorRoot()
 
             const insertOp =
                 {
@@ -43,11 +62,30 @@ describe('operation applier', () => {
                     }
                 }
 
-            await operationApplier[insertOp.type](insertOp as InsertNodeOperation, root, mockRuntime)
+            await operationApplier[insertOp.type](insertOp, root, mockRuntime)
 
-            const resultNode = await getNode([0,0,0], root)
-            const expectText = resultNode.get(FLUIDNODE_KEYS.TEXT)
+            const expectText = await getNodeText(root, [0,0,0])
+
             expect(expectText).toEqual('The first node')
+        });
+    });
+
+    describe('insert text operation', function () {
+        it('should insert text to a exist node', async () => {
+            const root = initEditorRoot()
+
+            const insertTextOp = {
+                type: 'insert_text',
+                offset: 17,
+                path: [0, 0],
+                text: ' and this insert text'
+            }
+
+            await operationApplier[insertTextOp.type](insertTextOp, root, mockRuntime)
+
+            const expectText = await getNodeText(root, [0,0])
+
+            expect(expectText).toEqual('This default text and this insert text')
         });
     });
 });
