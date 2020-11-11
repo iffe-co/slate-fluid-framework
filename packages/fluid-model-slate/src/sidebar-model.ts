0e41c4b572095ccf2c89fa83643b52aa5e7f0bbf
qainfeiqianlan <mqyangyang521@qq.com>
2020-11-11 17:05:55 +0800

    feat(sidebar): add transform path logic

--- packages/fluid-model-slate/src/sidebar-model.ts
+++ packages/fluid-model-slate/src/sidebar-model.ts
@@ -4,23 +4,28 @@ import {
   SharedObjectSequence,
   SharedString,
 } from '@fluidframework/sequence';
-import { Operation } from 'slate';
+import { Operation, InsertNodeOperation, RemoveNodeOperation } from 'slate';
 
-import { IFluidDataStoreRuntime } from '@fluidframework/datastore-definitions';
 import { IFluidHandle } from '@fluidframework/core-interfaces';
-import { getNodeFromCacheByHandle } from './dds-cache';
 import { Observable } from 'rxjs';
 
-const subPageCache = {};
-
 type RootType = SharedObjectSequence<IFluidHandle<SharedMap>>;
-
+type NodeType = { title: string; icon: string; src: string };
 class SidebarModel {
-  observable: Observable<Operation[]>;
+  public id: string;
 
-  onSidebarChange!: (ops: Operation[]) => void;
+  private readonly observable: Observable<Operation[]>;
+  private onSidebarChange!: (ops: Operation[]) => void;
 
-  constructor(private root: RootType) {
+  private currentData!: NodeType[];
+  processing: boolean = false;
+
+  private constructor(
+    private root: RootType,
+    private children: SharedMap[],
+    private applyToDocModel: (op: Operation) => void,
+  ) {
+    this.id = root.id;
     const _this = this;
     this.observable = new Observable(subscriber => {
       _this.onSidebarChange = (ops: Operation[]) => {
@@ -33,43 +38,135 @@ class SidebarModel {
     this.root.on(
       'sequenceDelta',
       (event: SequenceDeltaEvent, target: RootType) => {
-        // TODO: 当insert or remove的节点是page type的时候生成对应的op
-        const op = {} as any;
-        this.onSidebarChange([op]);
+        this.onChange();
       },
     );
   }
 
-  isSubPageNode(nodeHandle: IFluidHandle<SharedMap>) {
-    const node = getNodeFromCacheByHandle(nodeHandle);
+  onChange() {
+    if (!this.currentData || this.processing) {
+      return;
+    }
+
+    this.processing = true;
+    this.fetch().then(newData => {
+      const currentData = this.currentData;
+      this.currentData = newData;
+
+      const ops = this.diffData(currentData, newData);
+      this.onSidebarChange(ops);
+      this.processing = false;
+    });
+  }
+
+  createRemoveNodeOp(index: number, node: NodeType): Operation {
+    return ({
+      type: 'remove_node',
+      path: [index],
+      node,
+    } as unknown) as Operation;
+  }
+
+  createInsertNodeOp(index: number, node: NodeType): Operation {
+    return ({
+      type: 'insert_node',
+      path: [index],
+      node,
+    } as unknown) as Operation;
+  }
+
+  diffData(nodes1: NodeType[], nodes2: NodeType[]) {
+    const rangeSize =
+      nodes1.length > nodes2.length ? nodes1.length : nodes2.length;
+    const { removeOps, nodesAfterApplyOps } = this.diffRemove(nodes1, nodes2);
+    const insertOps = this.diffInsert(nodes1, nodes2);
+    const setNodeOps = this.diffSetNode(nodes1, nodes2);
+    return removeOps.concat(insertOps).concat(setNodeOps);
+  }
+
+  diffRemove(originNodes: NodeType[], newNodes: NodeType[]) {
+    const removeOps = originNodes
+      .map((n, i) => ({ index: i, value: n }))
+      .filter(v => !newNodes.find(newNode => newNode.src === v.value.src))
+      .map(({ index, value }) => this.createRemoveNodeOp(index, value));
+
+    const nodesAfterApplyOps = originNodes.filter(n =>
+      newNodes.find(newNode => newNode.src === n.src),
+    );
+
+    return { removeOps, nodesAfterApplyOps };
+  }
+
+  diffInsert(originNodes: NodeType[], newNodes: NodeType[]) {
+    const insertOps = newNodes
+      .map((n, i) => ({ index: i, value: n }))
+      .filter(
+        v => !originNodes.find(originNodes => originNodes.src === v.value.src),
+      )
+      .map(({ index, value }) => this.createInsertNodeOp(index, value));
+
+    return insertOps;
+  }
+
+  diffSetNode(originNodes: NodeType[], newNodes: NodeType[]) {
+    const setNodeOps = newNodes
+      .map((n, i) => ({ index: i, value: n }))
+      .filter(v =>
+        originNodes.find(originNodes => originNodes.src === v.value.src),
+      )
+      .map(({ index, value }, originIndex) =>
+        this.diffNode(originNodes[originIndex], value, index),
+      )
+      .filter(op => op);
+
+    return setNodeOps as any;
+  }
+
+  diffNode(originNode: NodeType, newNode: NodeType, index: number) {
+    if (originNode.title === newNode.title && originNode.src === newNode.src) {
+      return;
+    } else {
+      return {
+        type: 'set_node',
+        path: [index],
+        newProperties: newNode,
+        properties: originNode,
+      };
+    }
+  }
+
+  isSubPageNode(node: SharedMap) {
     return node.get('type') === 'page';
   }
 
-  getNodeValue(nodeHandle: IFluidHandle<SharedMap>) {
-    const node = getNodeFromCacheByHandle(nodeHandle);
-    const subPageId = node.get('src');
-    const subPage: { title: string; icon: string } = this.getSubPageFromCache(
+  getNodeValue(node: SharedMap) {
+    const src = node.get('src');
+    const subPage: { title: string; icon: string } = this.getSubPageFromManager(
       '',
-    ) as any; // will get sub page model from server
-    return { title: subPage.title, icon: subPage.icon };
-  }
-
-  getSubPageFromCache(id: string) {
-    return subPageCache[id];
-  }
-
-  filterSubPages(root: RootType) {
-    const subPages = this.root
-      .getItems(0)
-      .map((v, i) => ({ nodeHandle: v, index: i }))
-      .filter(({ nodeHandle }) => this.isSubPageNode(nodeHandle));
-    return subPages;
-  }
-
-  fetch() {
-    return this.filterSubPages(this.root).map(({ nodeHandle }) =>
-      this.getNodeValue(nodeHandle),
     );
+    return { title: subPage.title, icon: subPage.icon, src };
+  }
+
+  getSubPageFromManager(id: string) {
+    return {} as any;
+  }
+
+  async getChildren(root: RootType) {
+    return Promise.all(root.getRange(0).map(h => h.get()));
+  }
+
+  async getPageChildren(root: RootType) {
+    return (await this.getChildren(root)).filter(this.isSubPageNode);
+  }
+
+  async fetch() {
+    const children = (await this.getPageChildren(this.root)).map(
+      this.getNodeValue,
+    );
+    if (!this.currentData) {
+      this.currentData = children;
+    }
+    return children;
   }
 
   subscribe(): Observable<Operation[]> {
@@ -85,9 +182,61 @@ class SidebarModel {
   }
 
   applier = {
-    move_node: () => {},
-    remove_node: () => {},
-    insert_node: () => {},
+    move_node: async (op: InsertNodeOperation) => {
+      const children = await this.getChildren(this.root);
+      let [index] = op.path;
+      let [targetIndex] = op.newPath as number[];
+
+      let trueIndex = 0;
+      let trueTargetIndex = 0;
+      for (let i = 0; i < children.length; i++) {
+        if (this.isSubPageNode(children[i])) {
+          --index;
+          --targetIndex;
+        }
+        if (index === -1) {
+          trueIndex = i;
+        }
+        if (targetIndex === -1) {
+          trueTargetIndex = i;
+        }
+      }
+      this.applyToDocModel({
+        ...op,
+        path: [trueIndex],
+        newPath: [trueTargetIndex],
+      });
+    },
+    remove_node: async (op: InsertNodeOperation) => {
+      const children = await this.getChildren(this.root);
+      let [index] = op.path;
+      let i = 0;
+      for (; i < children.length; ++i) {
+        if (index === -1) {
+          break;
+        }
+        if (this.isSubPageNode(children[i])) {
+          --index;
+        }
+      }
+      const trueIndex = i;
+      this.applyToDocModel({ ...op, path: [trueIndex] });
+    },
+    insert_node: async (op: InsertNodeOperation) => {
+      const children = await this.getChildren(this.root);
+      let [index] = op.path;
+      let i = 0;
+      for (; i < children.length; ++i) {
+        if (index === 0) {
+          break;
+        }
+        if (this.isSubPageNode(children[i])) {
+          --index;
+        }
+      }
+      const trueIndex = index + i;
+      this.applyToDocModel({ ...op, path: [trueIndex] });
+    },
   };
 }
 
