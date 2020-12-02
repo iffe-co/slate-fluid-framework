0fe4e36869e09c79cd21e0f75157dcfb80dcb51e
qainfeiqianlan <mqyangyang521@qq.com>
2020-12-02 16:21:43 +0800

    remove examples and E2E test folder, will do a better solution later

--- E2E-test/src/create-new-model.ts
+++
@@ -1,5 +0,0 @@
-import {getSlateModel} from "./utils/model-getter";
-
-getSlateModel().then((res) => {
-    console.log(res.id)
-})
--- E2E-test/src/utils/editor-getter.ts
+++
@@ -1,9 +0,0 @@
-import {withSlateFluid} from "@solidoc/slate-fluid/src";
-import {createEditor} from "slate";
-import {SlateFluidModel} from "@solidoc/fluid-model-slate";
-
-export function getEditor(model: SlateFluidModel, initValue: any) {
-    const editor = withSlateFluid(createEditor(), model)
-    editor.children = initValue
-    return editor
-}
